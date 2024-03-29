8360fe50bd64536cd1807acd17bb4fcf6aaafa7d
Abuzar Mahmood <abuzarmahmood@gmail.com>
2024-03-29 09:33:41 -0400

    Add exponenents to script so that they don't have to be loaded from file

--- emg/gape_QDA_classifier/QDA_classifier.py
+++ emg/gape_QDA_classifier/QDA_classifier.py
@@ -1,13 +1,21 @@
-from scipy.io import loadmat
-
-# Load the gape algorithm results from Li et al. 2016
-a = loadmat('QDA_nostd_no_first.mat')
-a = a['important_coefficients'][0]
+# from scipy.io import loadmat
+# 
+# # Load the gape algorithm results from Li et al. 2016
+# a = loadmat('QDA_nostd_no_first.mat')
+# a = a['important_coefficients'][0]
 
 # Define a function that applies the QD algorithm to each individual movement,
 # with x = interval and y = duration.
 # Returns True or False based on if the QD evaluates to <0 or not
 
+a = [
+        18.71663787983482,
+        -4.21685571745406,
+        -0.14440351675879703,
+        0.2515943611518714,
+        0.019045297427119887,
+        6.687167660635821e-05,
+        ]
 
 def QDA(x, y):
     return (a[0] + a[1]*x + a[2]*y + a[3]*x**2 + a[4]*x*y + a[5]*y**2) < 0
