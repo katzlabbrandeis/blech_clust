0f74f12a3b4485dd3dbcc9e55f3f48b437b3d10f
Abuzar Mahmood <abuzarmahmood@gmail.com>
2025-02-03 12:39:00 -0500

    feat(utils): enhance PSTH calculations and data processing
    - Added `calc_conv_psth` function to calculate convolution rate of input arrays.
    - Added support for calculating inter-spike intervals with `calc_isi_array`.
    - Introduced half-Gaussian kernel convolution to modulate neural spike data.
    - Added parallel processing for convolution calculations to improve performance.
    - Enhanced plotting to include new calculation results such as half-Gaussian fit.
    - Updated data persistence methods with the inclusion of half-Gaussian rates.
    - Cleaned up code by commenting out unnecessary plot saving checks.
      
    These improvements provide better granularity and efficiency in processing neural data and visualizing results.

--- utils/basis_psth.py
+++ utils/basis_psth.py
@@ -2,6 +2,7 @@
 Using a raised cosine basis set to fit PSTHs
 """
 
+from joblib import Parallel, delayed
 from utils.ephys_data import visualize as vz
 from utils.ephys_data import ephys_data
 from utils import makeRaisedCosBasis as mrcb
@@ -11,9 +12,27 @@ import numpy as np
 import sys
 import pandas as pd
 from tqdm import tqdm, trange
+from scipy.optimize import curve_fit
 import os
 blech_dir = '/home/abuzarmahmood/Desktop/blech_clust'
 sys.path.append(blech_dir)
+
+
+def calc_conv_psth(this_array):
+    """
+    Calculate the convolution rate of the input array
+
+    args:
+        this_array: trials x time array
+
+    returns:
+        conv_psth: time
+    """
+    mean_spikes = this_array.mean(axis=0)
+    x = np.arange(len(mean_spikes))
+    conv_psth = np.convolve(mean_spikes, conv_kernel, mode='valid')
+    conv_x = np.convolve(x, conv_kernel, mode='valid')
+    return conv_psth, conv_x
 
 
 def fit_basis(basis_funcs, wanted_dat):
@@ -99,131 +118,354 @@ if not os.path.exists(artfact_dir):
 with open(data_dir_list_path, 'r') as file:
     data_dir_list = file.readlines()
 
-dat_frame_list = []
-
-for i, data_dir in enumerate(tqdm(data_dir_list)):
-
-    data_dir = data_dir.rstrip()
-    dat = ephys_data.ephys_data(data_dir)
-    dat.get_spikes()
-    taste_data = np.array(dat.spikes)
-
-    inds = np.array(
-        list(np.ndindex((len(taste_data), taste_data[0].shape[1]))))
-    taste_nrn_list = [taste_data[i, :, j] for i, j in inds]
-
-    this_frame = pd.DataFrame(inds, columns=['taste', 'neuron'])
-    this_frame['spikes'] = taste_nrn_list
-    this_frame['session'] = i
-    dat_frame_list.append(this_frame)
-
-    # wanted_dat = spike_array[0, :, 10, stim_t-pre_stim:stim_t+post_stim]
-
-dat_frame = pd.concat(dat_frame_list, ignore_index=True)
-
-basis_psth_list = []
-
-for ind, this_row in tqdm(dat_frame.iterrows()):
-    # save_path = os.path.join(
-    #         plot_dir,
-    #         f'{this_row["session"]}_{this_row["taste"]}_{this_row["neuron"]}.png')
-
-    if os.path.exists(save_path):
-        continue
-
-    wanted_dat = this_row['spikes']
-    wanted_dat = wanted_dat[:, stim_t-pre_stim:stim_t+post_stim]
-
-    # Linear basis
-    linear_fit = np.stack([fit_basis(linear_basis_funcs, this_trial)
-                          for this_trial in wanted_dat])
-
-    # Log basis
-    log_fit = np.stack([fit_basis(log_basis_funcs, this_trial)
-                       for this_trial in wanted_dat])
-
-    # Sigmoid basis
-    sigmoid_fit = np.stack([fit_basis(sigmoid_basis_funcs, this_trial)
-                           for this_trial in wanted_dat])
-
-    basis_psth_list.append([linear_fit, log_fit, sigmoid_fit])
-
-    # vmin = linear_fit.min()
-    # vmax = linear_fit.max()
-    # fig, ax = plt.subplots(4, 3, sharex=True, figsize=(15, 10))
-    # ax[0, 0].plot(linear_basis_funcs.T, color='k')
-    # ax[0, 0].plot(linear_basis_funcs.sum(axis=0), color='r', linewidth=3)
-    # ax[0, 0].set_title('Linearly spaced basis functions')
-    # ax[0, 1].plot(log_basis_funcs.T, color='k')
-    # ax[0, 1].plot(log_basis_funcs.sum(axis=0), color='r', linewidth=3)
-    # ax[0, 1].set_title('Logarithmically spaced basis functions')
-    # ax[1, 0].imshow(linear_fit, **img_kwargs, vmin=vmin, vmax=vmax)
-    # ax[1, 0].set_title('Linear fit')
-    # ax[1, 1].imshow(log_fit, **img_kwargs, vmin=vmin, vmax=vmax)
-    # ax[1, 1].set_title('Log fit')
-    # ax[2, 0] = vz.raster(ax[2, 0], wanted_dat, marker='|', color='k')
-    # ax[2, 0].set_title('Raster plot')
-    # ax[2, 1] = vz.raster(ax[2, 1], wanted_dat, marker='|', color='k')
-    # ax[2, 1].set_title('Linear fit')
-    # ax[0, 2].plot(sigmoid_basis_funcs.T, color='k')
-    # ax[0, 2].plot(sigmoid_basis_funcs.sum(axis=0), color='r', linewidth=3)
-    # ax[0, 2].set_title('Sigmoidally spaced basis functions')
-    # sigmoid_fit = np.stack([fit_basis(sigmoid_basis_funcs, this_trial)
-    #                        for this_trial in wanted_dat])
-    # ax[1, 2].imshow(sigmoid_fit, **img_kwargs, vmin=vmin, vmax=vmax)
-    # ax[1, 2].set_title('Sigmoid fit')
-    # ax[2, 2] = vz.raster(ax[2, 2], wanted_dat, marker='|', color='k')
-    # ax[2, 2].set_title('Sigmoid fit')
-    # ax[3, 0].plot(wanted_dat.sum(axis=0), alpha=0.5)
-    # ax[3, 0].plot(linear_fit.sum(axis=0), alpha=0.5)
-    # ax[3, 0].set_title('Linear fit')
-    # ax[3, 1].plot(wanted_dat.sum(axis=0), alpha=0.5)
-    # ax[3, 1].plot(log_fit.sum(axis=0), alpha=0.5)
-    # ax[3, 1].set_title('Log fit')
-    # ax[3, 2].plot(wanted_dat.sum(axis=0), alpha=0.5)
-    # ax[3, 2].plot(sigmoid_fit.sum(axis=0), alpha=0.5)
-    # ax[3, 2].set_title('Sigmoid fit')
-    # for this_ax in ax[1:].flatten():
-    #     this_ax.axvline(pre_stim, color='r', linestyle='--')
-    # plt.savefig(save_path)
-    # # plt.show()
-
-dat_frame['linear_fit'] = [this_fit[0] for this_fit in basis_psth_list]
-dat_frame['log_fit'] = [this_fit[1] for this_fit in basis_psth_list]
-dat_frame['sigmoid_fit'] = [this_fit[2] for this_fit in basis_psth_list]
-
-
-##############################
-# For each session, and neuron, plot all 4 tastes
-
 kern_len = 250
 conv_kernel = np.ones(kern_len)/kern_len
 
+basis_data_path = os.path.join(data_base_dir, 'basis_psth_data.pkl')
+if not os.path.exists(basis_data_path):
 
-def calc_conv_psth(this_array):
+    dat_frame_list = []
+
+    for i, data_dir in enumerate(tqdm(data_dir_list)):
+
+        data_dir = data_dir.rstrip()
+        dat = ephys_data.ephys_data(data_dir)
+        dat.get_spikes()
+        taste_data = np.array(dat.spikes)
+
+        inds = np.array(
+            list(np.ndindex((len(taste_data), taste_data[0].shape[1]))))
+        taste_nrn_list = [taste_data[i, :, j] for i, j in inds]
+
+        this_frame = pd.DataFrame(inds, columns=['taste', 'neuron'])
+        this_frame['spikes'] = taste_nrn_list
+        this_frame['session'] = i
+        dat_frame_list.append(this_frame)
+
+        # wanted_dat = spike_array[0, :, 10, stim_t-pre_stim:stim_t+post_stim]
+
+    dat_frame = pd.concat(dat_frame_list, ignore_index=True)
+
+    basis_psth_list = []
+
+    for ind, this_row in tqdm(dat_frame.iterrows()):
+        # save_path = os.path.join(
+        #         plot_dir,
+        #         f'{this_row["session"]}_{this_row["taste"]}_{this_row["neuron"]}.png')
+
+        # if os.path.exists(save_path):
+        #     continue
+
+        wanted_dat = this_row['spikes']
+        wanted_dat = wanted_dat[:, stim_t-pre_stim:stim_t+post_stim]
+
+        # Linear basis
+        linear_fit = np.stack([fit_basis(linear_basis_funcs, this_trial)
+                              for this_trial in wanted_dat])
+
+        # Log basis
+        log_fit = np.stack([fit_basis(log_basis_funcs, this_trial)
+                           for this_trial in wanted_dat])
+
+        # Sigmoid basis
+        sigmoid_fit = np.stack([fit_basis(sigmoid_basis_funcs, this_trial)
+                               for this_trial in wanted_dat])
+
+        basis_psth_list.append([linear_fit, log_fit, sigmoid_fit])
+
+        # vmin = linear_fit.min()
+        # vmax = linear_fit.max()
+        # fig, ax = plt.subplots(4, 3, sharex=True, figsize=(15, 10))
+        # ax[0, 0].plot(linear_basis_funcs.T, color='k')
+        # ax[0, 0].plot(linear_basis_funcs.sum(axis=0), color='r', linewidth=3)
+        # ax[0, 0].set_title('Linearly spaced basis functions')
+        # ax[0, 1].plot(log_basis_funcs.T, color='k')
+        # ax[0, 1].plot(log_basis_funcs.sum(axis=0), color='r', linewidth=3)
+        # ax[0, 1].set_title('Logarithmically spaced basis functions')
+        # ax[1, 0].imshow(linear_fit, **img_kwargs, vmin=vmin, vmax=vmax)
+        # ax[1, 0].set_title('Linear fit')
+        # ax[1, 1].imshow(log_fit, **img_kwargs, vmin=vmin, vmax=vmax)
+        # ax[1, 1].set_title('Log fit')
+        # ax[2, 0] = vz.raster(ax[2, 0], wanted_dat, marker='|', color='k')
+        # ax[2, 0].set_title('Raster plot')
+        # ax[2, 1] = vz.raster(ax[2, 1], wanted_dat, marker='|', color='k')
+        # ax[2, 1].set_title('Linear fit')
+        # ax[0, 2].plot(sigmoid_basis_funcs.T, color='k')
+        # ax[0, 2].plot(sigmoid_basis_funcs.sum(axis=0), color='r', linewidth=3)
+        # ax[0, 2].set_title('Sigmoidally spaced basis functions')
+        # sigmoid_fit = np.stack([fit_basis(sigmoid_basis_funcs, this_trial)
+        #                        for this_trial in wanted_dat])
+        # ax[1, 2].imshow(sigmoid_fit, **img_kwargs, vmin=vmin, vmax=vmax)
+        # ax[1, 2].set_title('Sigmoid fit')
+        # ax[2, 2] = vz.raster(ax[2, 2], wanted_dat, marker='|', color='k')
+        # ax[2, 2].set_title('Sigmoid fit')
+        # ax[3, 0].plot(wanted_dat.sum(axis=0), alpha=0.5)
+        # ax[3, 0].plot(linear_fit.sum(axis=0), alpha=0.5)
+        # ax[3, 0].set_title('Linear fit')
+        # ax[3, 1].plot(wanted_dat.sum(axis=0), alpha=0.5)
+        # ax[3, 1].plot(log_fit.sum(axis=0), alpha=0.5)
+        # ax[3, 1].set_title('Log fit')
+        # ax[3, 2].plot(wanted_dat.sum(axis=0), alpha=0.5)
+        # ax[3, 2].plot(sigmoid_fit.sum(axis=0), alpha=0.5)
+        # ax[3, 2].set_title('Sigmoid fit')
+        # for this_ax in ax[1:].flatten():
+        #     this_ax.axvline(pre_stim, color='r', linestyle='--')
+        # plt.savefig(save_path)
+        # # plt.show()
+
+    dat_frame['linear_fit'] = [this_fit[0] for this_fit in basis_psth_list]
+    dat_frame['log_fit'] = [this_fit[1] for this_fit in basis_psth_list]
+    dat_frame['sigmoid_fit'] = [this_fit[2] for this_fit in basis_psth_list]
+
+    ##############################
+    # For each session, and neuron, plot all 4 tastes
+
+    conv_psth_list = [
+        calc_conv_psth(this_row['spikes']) for ind, this_row in dat_frame.iterrows()]
+
+    dat_frame['conv_psth'] = [this_psth[0] for this_psth in conv_psth_list]
+
+    dat_frame.to_pickle(basis_data_path)
+else:
+    dat_frame = pd.read_pickle(basis_data_path)
+
+conv_x = calc_conv_psth(dat_frame.iloc[0]['spikes'])[1]
+
+##############################
+# Get session = 9, unit = 10
+this_frame = dat_frame[(dat_frame['session'] == 9) &
+                       (dat_frame['neuron'] == 10)]
+
+spikes = np.stack(this_frame.spikes.to_numpy())
+cat_spikes = np.concatenate(spikes, axis=0)
+
+vz.raster(None, cat_spikes, marker='|', color='k')
+plt.show()
+
+
+def calc_isi_array(cat_spikes):
     """
-    Calculate the convolution rate of the input array
+    Calculate the inter-spike interval array
 
     args:
-        this_array: trials x time array
+        cat_spikes: concatenated spikes array
 
     returns:
-        conv_psth: time
+        isi_array: inter-spike interval array
     """
-    mean_spikes = this_array.mean(axis=0)
-    x = np.arange(len(mean_spikes))
-    conv_psth = np.convolve(mean_spikes, conv_kernel, mode='valid')
-    conv_x = np.convolve(x, conv_kernel, mode='valid')
-    return conv_psth, conv_x
+    spike_inds = [np.where(this_trial)[0] for this_trial in cat_spikes]
+    isi_list = [np.diff(this_trial) for this_trial in spike_inds]
+
+    isi_array = np.zeros(cat_spikes.shape)
+    for trial_ind, (trial_spikes, trial_isis) in enumerate(zip(spike_inds, isi_list)):
+        for i, this_isi in enumerate(trial_isis):
+            isi_array[trial_ind][trial_spikes[i]:trial_spikes[i+1]] = this_isi
+    return isi_array
 
 
-conv_psth_list = [
-    calc_conv_psth(this_row['spikes']) for ind, this_row in dat_frame.iterrows()]
-conv_x = conv_psth_list[0][1]
+isi_array = calc_isi_array(cat_spikes)
 
-dat_frame['conv_psth'] = [this_psth[0] for this_psth in conv_psth_list]
+# # Calculate inter-spike intervals
+# spike_inds = [np.where(this_trial)[0] for this_trial in cat_spikes]
+# isi_list = [np.diff(this_trial) for this_trial in spike_inds]
+#
+# isi_array = np.zeros(cat_spikes.shape)
+# for trial_ind, (trial_spikes, trial_isis) in enumerate(zip(spike_inds, isi_list)):
+#     for i, this_isi in enumerate(trial_isis):
+#         isi_array[trial_ind][trial_spikes[i]:trial_spikes[i+1]] = this_isi
+#
+fig, ax = plt.subplots(2, 1, sharex=True)
+ax[0].imshow(isi_array, aspect='auto', origin='lower', interpolation='nearest')
+ax[1].plot(isi_array.mean(axis=0))
+plt.show()
 
-dat_frame.to_pickle(os.path.join(data_base_dir, 'basis_psth_data.pkl'))
+pre_mean_isi = isi_array.mean(axis=0)[stim_t-pre_stim:stim_t].mean()
+
+
+def sigmoid(x, a, b):
+    return 1 / (1 + np.exp(-a*(x-b)))
+
+
+# Fit sigmoid to post
+post_isi = isi_array.mean(axis=0)[stim_t:stim_t+post_stim]
+post_isi = post_isi - post_isi.min()
+post_isi = post_isi / post_isi.max()
+
+post_x = np.arange(len(post_isi))
+popt, pcov = curve_fit(sigmoid, post_x, post_isi, p0=[0.025, 500])
+
+plt.plot(post_x, post_isi)
+plt.plot(post_x, sigmoid(post_x, *popt))
+plt.show()
+
+# sum_isi = np.zeros(cat_spikes.shape[1])
+# count_isi = np.zeros(cat_spikes.shape[1])
+#
+# for trial_inds, trial_isis in zip(spike_inds, isi_list):
+#     mean_isi[trial_inds[:-1]] += trial_isis
+#     count_isi[trial_inds[:-1]] += 1
+#
+# mean_isi = mean_isi / count_isi
+#
+# plt.plot(mean_isi, 'x')
+# plt.show()
+#
+# # Median filter
+# kern_len = 10
+# med_isi = np.array([np.nanmedian(mean_isi[max(0, i-kern_len):min(len(mean_isi), i+kern_len)]) \
+#         for i in range(len(mean_isi))])
+#
+#
+# plt.plot(mean_isi, 'x')
+# plt.plot(med_isi, 'x')
+# plt.show()
+
+##############################
+# Example regression
+
+y = np.exp((basis_x + 500) / 1000)
+y = y / y.max()
+y_sigmoid_fit = fit_basis(sigmoid_basis_funcs, y)
+
+plt.plot(y, label='True')
+plt.plot(y_sigmoid_fit, label='Fit')
+plt.legend()
+plt.show()
+
+##############################
+# Convolve with half-gaussian kernel with std = proportional to mean ISI
+isi_mean = isi_array.mean(axis=0)
+
+plt.plot(isi_mean)
+plt.show()
+
+
+def half_gaussian(x, mu, sigma):
+    return np.exp(-(x-mu)**2/(2*sigma**2)) * (x < mu)
+
+
+def gen_half_gaussian_kern_array(isi_mean, isi_sigma_ratio=1):
+    """
+    Generate a kernel array for half-gaussian convolution
+
+    args:
+        isi_mean: mean ISI array
+        isi_sigma_ratio: ratio of sigma to mean ISI (higher = wider kernel)
+
+    returns:
+        kern_array: kernel array
+    """
+    kern_array = np.zeros((len(isi_mean), len(isi_mean)))
+    for i, this_isi in enumerate(isi_mean):
+        this_kern = half_gaussian(
+            np.arange(len(isi_mean)), i, this_isi*isi_sigma_ratio)
+        # Normalize
+        this_kern /= this_kern.sum()
+        kern_array[i] = this_kern
+    return kern_array
+
+
+kern_array = gen_half_gaussian_kern_array(isi_mean, isi_sigma_ratio=1)
+
+
+def calc_half_gauss_rates(spike_array):
+    """
+    Calculate the half-gaussian convolved rates
+
+    args:
+        cat_spikes: concatenated spikes array
+        kern_array: kernel array
+
+    returns:
+        half_gauss_rates: convolved rates array
+    """
+    isi_array = calc_isi_array(spike_array)
+    mean_isi = isi_array.mean(axis=0)
+    # Generate kernel array
+    kern_array = gen_half_gaussian_kern_array(mean_isi, isi_sigma_ratio=1)
+
+    half_gauss_rates = spike_array @ (kern_array.T)
+    # Fill nans with 0
+    half_gauss_rates = np.nan_to_num(half_gauss_rates)
+    return half_gauss_rates
+
+# rates = calc_half_gauss_rates(spike_array)
+#
+# fig, ax = plt.subplots(2, 1, sharex=True)
+# ax[0] = vz.raster(ax[0], spike_array, marker='|', color='k')
+# ax[1].imshow(rates, aspect='auto', origin='lower', vmax = 0.1)
+# plt.show()
+#
+# plt.imshow(kern_array, aspect='equal', origin='lower')
+# plt.show()
+
+
+half_gauss_rates = cat_spikes @ (kern_array.T)
+# Fill nans with 0
+half_gauss_rates = np.nan_to_num(half_gauss_rates)
+
+plt.imshow(half_gauss_rates, aspect='auto', origin='lower')
+plt.show()
+
+fig, ax = plt.subplots(3, 1, sharex=True)
+ax[0] = vz.raster(ax[0], cat_spikes, marker='|', color='k')
+ax[1].imshow(half_gauss_rates, aspect='auto', origin='lower', vmax=0.1)
+ax[2].plot(half_gauss_rates.T, alpha=0.05, color='k')
+ax[2].set_ylim(0, 0.1)
+ax[2].set_xlim(1500, 4000)
+plt.show()
+
+taste_half_gauss_rates = np.tensordot(spikes, kern_array, axes=([-1], [1]))
+mean_taste_half_gauss_rates = taste_half_gauss_rates.mean(axis=1)
+
+sigmoid_fit = np.stack(this_frame.sigmoid_fit.to_numpy())
+mean_sigmoid_fit = sigmoid_fit.mean(axis=1)
+
+fig, ax = plt.subplots(2, 1, sharex=True)
+ax[0].plot(mean_sigmoid_fit.T)
+ax[1].plot(mean_taste_half_gauss_rates.T[stim_t - pre_stim:stim_t+post_stim])
+plt.show()
+
+
+############################################################
+############################################################
+
+
+def parallelize(func, iterable, n_jobs=-1):
+    """
+    Parallelize a function over an iterable
+
+    args:
+        func: function to parallelize
+        iterable: iterable to apply function to
+        n_jobs: number of jobs to run in parallel
+
+    returns:
+        results: list of results
+    """
+    results = Parallel(n_jobs=n_jobs)(delayed(func)(i) for i in tqdm(iterable))
+    return results
+
+
+# Add half-gaussian convolution to the dat_frame
+spikes_list = dat_frame.spikes.to_list()
+# Cut down to just the relevant part
+half_gauss_rates_list = parallelize(calc_half_gauss_rates, spikes_list)
+half_gauss_rates_list = [this_rates[:, stim_t-pre_stim:stim_t+post_stim]
+                         for this_rates in half_gauss_rates_list]
+
+# for ind, this_row in tqdm(dat_frame.iterrows()):
+#     wanted_dat = this_row['spikes']
+#     half_gauss_rates = calc_half_gauss_rates(wanted_dat)
+#     half_gauss_rates_list.append(half_gauss_rates)
+#
+dat_frame['half_gauss_rates'] = half_gauss_rates_list
+
+# dat_frame.to_pickle(basis_data_path)
+# dat_frame.to_hdf(basis_data_path.replace('.pkl', '.h5'), key='df')
+
+############################################################
+############################################################
 
 this_plot_dir = os.path.join(data_base_dir, 'taste_plots')
 if not os.path.exists(this_plot_dir):
@@ -235,18 +477,20 @@ for (session, neuron), this_frame in tqdm(dat_frame.groupby(['session', 'neuron'
     save_path = os.path.join(
         this_plot_dir,
         f'{session}_{neuron}.png')
-    if os.path.exists(save_path):
-        continue
+    # if os.path.exists(save_path):
+    #     continue
 
     plot_dat = [
         this_frame.conv_psth.to_numpy(),
         this_frame.linear_fit.to_numpy(),
         this_frame.log_fit.to_numpy(),
         this_frame.sigmoid_fit.to_numpy(),
+        this_frame.half_gauss_rates.to_numpy()
     ]
     plot_dat = [np.stack(this_dat) for this_dat in plot_dat]
-    dat_names = ['Convolved PSTH', 'Linear fit', 'Log fit', 'Sigmoid fit']
-    fig, ax = plt.subplots(5, 1, sharex=True, figsize=(10, 20))
+    dat_names = ['Convolved PSTH', 'Linear fit', 'Log fit',
+                 'Sigmoid fit', 'ISI-modulated Half-Gaussian fit']
+    fig, ax = plt.subplots(len(plot_dat)+1, 1, sharex=True, figsize=(10, 20))
     for i, this_dat in enumerate(plot_dat):
         this_dat_name = dat_names[i]
         if this_dat_name == 'Convolved PSTH':
