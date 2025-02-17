1bc4e2ddaf7814fc25975d0ed39ee4cea8eab250
Abuzar Mahmood <abuzarmahmood@gmail.com>
2025-02-17 15:24:14 -0500

    feat(pipeline): enhance RNN task to handle separate tastes
    - Added a `separate_tastes` parameter to the `run_rnn` function for more granular data processing.
    - Updated calls to `run_rnn` to include various combinations of `separate_regions` and `separate_tastes` options.
    - This change allows for more flexible configurations in the RNN inference process based on data characteristics.

--- pipeline_testing/prefect_pipeline.py
+++ pipeline_testing/prefect_pipeline.py
@@ -365,7 +365,7 @@ def run_gapes_Li(data_dir):
 
 
 @task(log_prints=True)
-def run_rnn(data_dir, separate_regions=False):
+def run_rnn(data_dir, separate_regions=False, separate_tastes=False):
     """Run RNN firing rate inference"""
     script_name = 'utils/infer_rnn_rates.py'
     # Use 100 training steps for testing
@@ -373,6 +373,8 @@ def run_rnn(data_dir, separate_regions=False):
             "--train_steps", "100", "--retrain"]
     if separate_regions:
         args.append("--separate_regions")
+    if separate_tastes:
+        args.append("--separate_tastes")
     process = Popen(args,
                     stdout=PIPE, stderr=PIPE)
     stdout, stderr = process.communicate()
@@ -418,8 +420,10 @@ def run_spike_test(data_dir):
     quality_assurance(data_dir)
     units_plot(data_dir)
     units_characteristics(data_dir)
-    run_rnn(data_dir, separate_regions=False)
-    run_rnn(data_dir, separate_regions=True)
+    run_rnn(data_dir, separate_regions=False,   separate_tastes=False)
+    run_rnn(data_dir, separate_regions=True,    separate_tastes=False)
+    run_rnn(data_dir, separate_regions=False,   separate_tastes=True)
+    run_rnn(data_dir, separate_regions=True,    separate_tastes=True)
 
 
 @flow(log_prints=True)
