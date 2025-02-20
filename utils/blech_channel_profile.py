c57a8e477c2eff9d674f684aaf1bde85151020f4
Abuzar Mahmood <abuzarmahmood@gmail.com>
2025-02-19 20:53:33 -0500

    refactor(plot): reorganize plotting logic for amplifier and digital input signals
    - Consolidated redundant plotting code by reordering the logic structure.
    - Improved readability by grouping related plotting routines together.
    - Ensured both "one file per channel" and "one file per signal type" options are handled succinctly.

--- utils/blech_channel_profile.py
+++ utils/blech_channel_profile.py
@@ -55,79 +55,79 @@ def plot_channels(dir_path, output_dir):
         raise Exception("Couldn't find amp*.dat files in dir" + "\n" +
                         f"{dir_path}")
 
+    # Plot files
+    print("Now plotting ampilfier signals")
+    downsample = 100
+    row_lim = 8
+    if file_type == ['one file per channel']:
+        row_num = np.min((row_lim, len(amp_files)))
+        col_num = int(np.ceil(len(amp_files)/row_num))
+        # Create plot
+        fig, ax = plt.subplots(row_num, col_num,
+                               sharex=True, sharey=True, figsize=(15, 10))
+        for this_file, this_ax in tqdm(zip(amp_files, ax.flatten())):
+            data = np.fromfile(this_file, dtype=np.dtype('int16'))
+            this_ax.plot(data[::downsample])
+            this_ax.set_ylabel("_".join(os.path.basename(this_file)
+                                        .split('.')[0].split('-')[1:]))
+        plt.suptitle('Amplifier Data')
+        fig.savefig(os.path.join(plot_dir, 'amplifier_data'))
+        plt.close(fig)
+    elif file_type == ['one file per signal type']:
+        amplifier_data = np.fromfile(amp_files[0], dtype=np.dtype('uint16'))
+        num_electrodes = int(len(amplifier_data)/num_recorded_samples)
+        amp_reshape = np.reshape(amplifier_data, (int(
+            len(amplifier_data)/num_electrodes), num_electrodes)).T
+        row_num = np.min((row_lim, num_electrodes))
+        col_num = int(np.ceil(num_electrodes/row_num))
+        # Create plot
+        fig, ax = plt.subplots(row_num, col_num,
+                               sharex=True, sharey=True, figsize=(15, 10))
+        for e_i in tqdm(range(num_electrodes)):
+            data = amp_reshape[e_i, :]
+            ax_i = plt.subplot(row_num, col_num, e_i+1)
+            ax_i.plot(data[::downsample])
+            ax_i.set_ylabel('amp_' + str(e_i))
+        plt.suptitle('Amplifier Data')
+        fig.savefig(os.path.join(plot_dir, 'amplifier_data'))
+        plt.close(fig)
 
-# Plot files
-print("Now plotting ampilfier signals")
-downsample = 100
-row_lim = 8
-if file_type == ['one file per channel']:
-    row_num = np.min((row_lim, len(amp_files)))
-    col_num = int(np.ceil(len(amp_files)/row_num))
-    # Create plot
-    fig, ax = plt.subplots(row_num, col_num,
-                           sharex=True, sharey=True, figsize=(15, 10))
-    for this_file, this_ax in tqdm(zip(amp_files, ax.flatten())):
-        data = np.fromfile(this_file, dtype=np.dtype('int16'))
-        this_ax.plot(data[::downsample])
-        this_ax.set_ylabel("_".join(os.path.basename(this_file)
-                                    .split('.')[0].split('-')[1:]))
-    plt.suptitle('Amplifier Data')
-    fig.savefig(os.path.join(plot_dir, 'amplifier_data'))
-    plt.close(fig)
-elif file_type == ['one file per signal type']:
-    amplifier_data = np.fromfile(amp_files[0], dtype=np.dtype('uint16'))
-    num_electrodes = int(len(amplifier_data)/num_recorded_samples)
-    amp_reshape = np.reshape(amplifier_data, (int(
-        len(amplifier_data)/num_electrodes), num_electrodes)).T
-    row_num = np.min((row_lim, num_electrodes))
-    col_num = int(np.ceil(num_electrodes/row_num))
-    # Create plot
-    fig, ax = plt.subplots(row_num, col_num,
-                           sharex=True, sharey=True, figsize=(15, 10))
-    for e_i in tqdm(range(num_electrodes)):
-        data = amp_reshape[e_i, :]
-        ax_i = plt.subplot(row_num, col_num, e_i+1)
-        ax_i.plot(data[::downsample])
-        ax_i.set_ylabel('amp_' + str(e_i))
-    plt.suptitle('Amplifier Data')
-    fig.savefig(os.path.join(plot_dir, 'amplifier_data'))
-    plt.close(fig)
+    print("Now plotting digital input signals")
+    if file_type == ['one file per channel']:
+        fig, ax = plt.subplots(len(digin_files),
+                               sharex=True, sharey=True, figsize=(8, 10))
+        for this_file, this_ax in tqdm(zip(digin_files, ax.flatten())):
+            data = np.fromfile(this_file, dtype=np.dtype('uint16'))
+            this_ax.plot(data[::downsample])
+            this_ax.set_ylabel("_".join(os.path.basename(this_file)
+                                        .split('.')[0].split('-')[1:]))
+        plt.suptitle('DIGIN Data')
+        fig.savefig(os.path.join(plot_dir, 'digin_data'))
+        plt.close(fig)
+    elif file_type == ['one file per signal type']:
+        d_inputs = np.fromfile(digin_files[0], dtype=np.dtype('uint16'))
+        d_inputs_str = d_inputs.astype('str')
+        d_in_str_int = d_inputs_str.astype('int64')
+        d_diff = np.diff(d_in_str_int)
+        dig_in = list(np.unique(np.abs(d_diff)) - 1)
+        dig_in.remove(-1)
+        num_dig_ins = len(dig_in)
+        dig_inputs = np.zeros((num_dig_ins, len(d_inputs)))
+        for n_i in range(num_dig_ins):
+            start_ind = np.where(d_diff == n_i + 1)[0]
+            end_ind = np.where(d_diff == -1*(n_i + 1))[0]
+            for s_i in range(len(start_ind)):
+                dig_inputs[n_i, start_ind[s_i]:end_ind[s_i]] = 1
+        fig, ax = plt.subplots(num_dig_ins, 1,
+                               sharex=True, sharey=True, figsize=(8, 10))
+        for d_i in tqdm(range(num_dig_ins)):
+            ax_i = plt.subplot(num_dig_ins, 1, d_i+1)
+            ax_i.plot(dig_inputs[d_i, ::downsample])
+            ax_i.set_ylabel('Dig_in_' + str(dig_in[d_i]))
+        plt.suptitle('DIGIN Data')
+        fig.savefig(os.path.join(plot_dir, 'digin_data'))
+        plt.close(fig)
 
-print("Now plotting digital input signals")
-if file_type == ['one file per channel']:
-    fig, ax = plt.subplots(len(digin_files),
-                           sharex=True, sharey=True, figsize=(8, 10))
-    for this_file, this_ax in tqdm(zip(digin_files, ax.flatten())):
-        data = np.fromfile(this_file, dtype=np.dtype('uint16'))
-        this_ax.plot(data[::downsample])
-        this_ax.set_ylabel("_".join(os.path.basename(this_file)
-                                    .split('.')[0].split('-')[1:]))
-    plt.suptitle('DIGIN Data')
-    fig.savefig(os.path.join(plot_dir, 'digin_data'))
-    plt.close(fig)
-elif file_type == ['one file per signal type']:
-    d_inputs = np.fromfile(digin_files[0], dtype=np.dtype('uint16'))
-    d_inputs_str = d_inputs.astype('str')
-    d_in_str_int = d_inputs_str.astype('int64')
-    d_diff = np.diff(d_in_str_int)
-    dig_in = list(np.unique(np.abs(d_diff)) - 1)
-    dig_in.remove(-1)
-    num_dig_ins = len(dig_in)
-    dig_inputs = np.zeros((num_dig_ins, len(d_inputs)))
-    for n_i in range(num_dig_ins):
-        start_ind = np.where(d_diff == n_i + 1)[0]
-        end_ind = np.where(d_diff == -1*(n_i + 1))[0]
-        for s_i in range(len(start_ind)):
-            dig_inputs[n_i, start_ind[s_i]:end_ind[s_i]] = 1
-    fig, ax = plt.subplots(num_dig_ins, 1,
-                           sharex=True, sharey=True, figsize=(8, 10))
-    for d_i in tqdm(range(num_dig_ins)):
-        ax_i = plt.subplot(num_dig_ins, 1, d_i+1)
-        ax_i.plot(dig_inputs[d_i, ::downsample])
-        ax_i.set_ylabel('Dig_in_' + str(dig_in[d_i]))
-    plt.suptitle('DIGIN Data')
-    fig.savefig(os.path.join(plot_dir, 'digin_data'))
-    plt.close(fig)
 if __name__ == '__main__':
     import argparse
     import sys
