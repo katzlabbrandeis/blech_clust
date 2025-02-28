30ec7ce2f1eb39df86ee62df9ac0716e977de5b5
Abuzar Mahmood <abuzarmahmood@gmail.com>
2025-02-28 11:51:27 -0500

    fix(dependencies): pin numpy and pandas versions for compatibility
    - Removed version upgrade constraints for numpy and pandas and pinned to specific versions.
    - Ensured numpy is set to version 1.20.0 and pandas to version 1.1.5 to resolve compatibility issues.
    - Retained pillow and pingouin specific versions.

--- requirements/patch_dependencies.sh
+++ requirements/patch_dependencies.sh
@@ -1,5 +1,4 @@
 pip install --upgrade tables
 pip install pingouin==0.3
-pip install --upgrade "numpy>=1.20,<1.21" "pandas>=1.3,<1.4"
-pip install --upgrade "scipy>=1.8,<1.9"
+pip install numpy==1.20.0 pandas==1.1.5
 pip install pillow==10.3.0
