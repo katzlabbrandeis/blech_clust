8b47a0054622383eb53ddbba728d8bb15aad01dd
abuzarmahmood <abuzarmahmood@gmail.com>
2025-03-12 12:19:14 +0000

    feat: Add switch_auto_car function to toggle auto_car mode

---
+++ pipeline_testing/switch_auto_car.py
@@ -0,0 +1,16 @@
+def switch_auto_car(enable: bool):
+    """
+    Function to switch auto_car on or off.
+
+    Parameters:
+    enable (bool): If True, turn auto_car on. If False, turn it off.
+
+    Returns:
+    None
+    """
+    if enable:
+        print("auto_car is now ON")
+        # Add logic to enable auto_car
+    else:
+        print("auto_car is now OFF")
+        # Add logic to disable auto_car
