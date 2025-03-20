990670d641419354ffe7cb7fc9704ea4cee9ba3f
Abuzar Mahmood <abuzarmahmood@gmail.com>
2025-03-20 16:22:00 -0400

    refactor(train): optimize and modularize PCA and RNN training loop
    - Removed repetitive code by looping over trials, utilising lists for storing temporary results.
    - Introduced Linear Regression to align and transform spike train data before PCA.
    - Adjusted RNN training to support multiple sub-models, adapting input sizes accordingly.
    - Enhanced training feedback by updating plot logic for training and cross-validation loss.
    - Replaced sequential variable updates with list operations to handle data transformations and model inputs.
    - Changed plotting strategy for better visualization with subplots and increased resolution with `ax` figures.

--- utils/foundational_rnn/src/train_model.py
+++ utils/foundational_rnn/src/train_model.py
@@ -1,5 +1,6 @@
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.decomposition import PCA
+from sklearn.linear_model import LinearRegression
 import torch.nn as nn
 import os
 import sys
@@ -92,121 +93,62 @@ stim_ind = 500 // 25
 ############################################################
 ############################################################
 
-i = 0
-this_row = out_frame.iloc[i]
-stim_dur = this_row.stimulus_duration[this_row.taste]
-stim_dur_inds = int(stim_dur // 25)
+binned_spikes_list = []
+mean_binned_spikes_list = []
 
-binned_spikes = this_row.spike_train.copy()
+# Perform PCA on data and perform alignment
 
-##############################
-# Begin copying from infer_rnn_rates.py
-##############################
+for i in range(len(out_frame)):
+    this_row = out_frame.iloc[i]
+    stim_dur = this_row.stimulus_duration[this_row.taste]
+    stim_dur_inds = int(stim_dur // 25)
 
-# Reshape to (seq_len, batch, input_size)
-# seq_len = time
-# batch = trials
-# input_size = neurons
-inputs = binned_spikes.copy()
-inputs = np.moveaxis(inputs, -1, 0)
+    binned_spikes = this_row.spike_train.copy()
+    mean_binned_spikes = binned_spikes.mean(axis=0)
 
-##############################
-# Perform PCA on data
-# If PCA is performed on raw data, higher firing neurons will dominate
-# the latent space
-# Therefore, perform PCA on zscored data
+    binned_spikes_list.append(binned_spikes)
+    mean_binned_spikes_list.append(mean_binned_spikes)
 
-inputs_long = inputs.reshape(-1, inputs.shape[-1])
 
-# Perform standard scaling
-scaler = StandardScaler()
-# scaler = MinMaxScaler()
-inputs_long = scaler.fit_transform(inputs_long)
+cat_mean_binned_spikes = np.concatenate(mean_binned_spikes_list, axis=0)
 
-print('Performing PCA')
-# Perform PCA and get 95% explained variance
-pca_obj = PCA(n_components=8)
-inputs_pca = pca_obj.fit_transform(inputs_long)
-n_components = inputs_pca.shape[-1]
+pca_obj = PCA(n_components=8, whiten=True)
+cat_mean_pca = pca_obj.fit_transform(cat_mean_binned_spikes.T).T
 
-# # Scale the PCA outputs
-# pca_scaler = StandardScaler()
-# inputs_pca = pca_scaler.fit_transform(inputs_pca)
+# vz.imshow(cat_mean_binned_spikes)
+# plt.show()
+# vz.imshow(cat_mean_pca)
+# plt.show()
 
-inputs_trial_pca = inputs_pca.reshape(
-    inputs.shape[0], -1, n_components)
+transform_list = []
+# Learn transformation matrices
+for this_mean in mean_binned_spikes_list:
+    lr = LinearRegression()
+    # this_mean shape: (neurons, time)
+    # cat_mean_pca shape: (pca, time)
+    lr.fit(this_mean.T, cat_mean_pca.T)
+    transform_list.append(lr)
 
-# shape: (time, trials, pca_components)
-inputs = inputs_trial_pca.copy()
+binned_spikes_long_list = []
+aligned_spikes_long_list = []
+aligned_spikes_trials_list = []
+for i, binned_spikes in enumerate(binned_spikes_list):
+    binned_spikes_long = binned_spikes.swapaxes(0, 1)
+    binned_spikes_long = binned_spikes_long.reshape(
+        binned_spikes_long.shape[0], -1)
+    binned_spikes_long_list.append(binned_spikes_long)
 
-##############################
+    # Perform alignment
+    aligned_spikes = transform_list[i].predict(binned_spikes_long.T).T
+    aligned_spikes_long_list.append(aligned_spikes)
 
-# Add stim time as external input
-# Shape: (time, trials, 1)
-stim_time = np.zeros((inputs.shape[0], inputs.shape[1]))
-stim_time[stim_ind:stim_ind + stim_dur_inds, :] = 1
+    # Convert back to trials
+    aligned_spikes_trials = aligned_spikes.reshape(
+        aligned_spikes.shape[0], binned_spikes.shape[0], binned_spikes.shape[-1])
+    aligned_spikes_trials = aligned_spikes_trials.swapaxes(0, 1)
 
-# Also add trial number as external input
-# Scale trial number to be between 0 and 1
-trial_num = np.arange(binned_spikes.shape[0])
-trial_num_scaled = trial_num / trial_num.max()
-trial_num_broad = np.broadcast_to(trial_num_scaled, inputs.shape[:2])
+    aligned_spikes_trials_list.append(aligned_spikes_trials)
 
-# Shape: (time, trials, pca_components + 1)
-inputs_plus_context = np.concatenate(
-    [
-        inputs,
-        stim_time[:, :, None],
-        trial_num_broad[:, :, None]
-    ],
-    axis=-1)
-
-############################################################
-# Train model
-############################################################
-if torch.cuda.is_available():
-    device = torch.device("cuda:0")
-    print("Running on the GPU")
-else:
-    device = torch.device("cpu")
-    print("Running on the CPU")
-
-input_size = inputs_plus_context.shape[-1]
-# We don't want to predict the stim time or trial number
-output_size = inputs_plus_context.shape[-1] - 2
-
-# Instead of predicting activity in the SAME time-bin,
-# predict activity in the NEXT time-bin
-# Forcing the model to learn temporal dependencies
-forecast_bins = 1
-inputs_plus_context = inputs_plus_context[:-forecast_bins]
-inputs = inputs[forecast_bins:]
-
-# (seq_len * batch, output_size)
-labels = torch.from_numpy(inputs).type(torch.float32)
-# (seq_len, batch, input_size)
-inputs = torch.from_numpy(inputs_plus_context).type(torch.float)
-
-# Split into train and test
-train_inds = np.random.choice(
-    np.arange(inputs.shape[1]),
-    int(0.9 * inputs.shape[1]),
-    replace=False)
-test_inds = np.setdiff1d(np.arange(inputs.shape[1]), train_inds)
-
-train_inputs = inputs[:, train_inds]
-train_labels = labels[:, train_inds]
-test_inputs = inputs[:, test_inds]
-test_labels = labels[:, test_inds]
-
-train_inputs = train_inputs.to(device)
-train_labels = train_labels.to(device)
-test_inputs = test_inputs.to(device)
-test_labels = test_labels.to(device)
-
-##############################
-# Train
-##############################
 shared_rnn = autoencoderRNN(
     input_size=input_size,
     hidden_size=8,
@@ -215,46 +157,177 @@ shared_rnn = autoencoderRNN(
     dropout=0.2,
 )
 
-net = RNN_Wrapper(
-    n_io_layers=3,
-    input_size=input_size,
-    output_size=output_size,
-    shared_rnn=shared_rnn,
-)
+net_list = []
+train_inputs_list = []
+train_labels_list = []
+test_inputs_list = []
+test_labels_list = []
 
-net.to(device)
+for i, input_array in enumerate(aligned_spikes_trials_list):
 
-train_steps = 50_000
+    ##############################
+    # Begin copying from infer_rnn_rates.py
+    ##############################
+    # Reshape to (seq_len, batch, input_size)
+    # seq_len = time
+    # batch = trials
+    # input_size = neurons
+    inputs = input_array.copy()
+    # inputs = binned_spikes.copy()
+    inputs = np.moveaxis(inputs, -1, 0)
+
+    ##############################
+    # Perform PCA on data
+    # If PCA is performed on raw data, higher firing neurons will dominate
+    # the latent space
+    # Therefore, perform PCA on zscored data
+
+    # inputs_long = inputs.reshape(-1, inputs.shape[-1])
+    #
+    # # Perform standard scaling
+    # scaler = StandardScaler()
+    # # scaler = MinMaxScaler()
+    # inputs_long = scaler.fit_transform(inputs_long)
+    #
+    # print('Performing PCA')
+    # # Perform PCA and get 95% explained variance
+    # pca_obj = PCA(n_components=8)
+    # inputs_pca = pca_obj.fit_transform(inputs_long)
+    # n_components = inputs_pca.shape[-1]
+    #
+    # # # Scale the PCA outputs
+    # # pca_scaler = StandardScaler()
+    # # inputs_pca = pca_scaler.fit_transform(inputs_pca)
+    #
+    # inputs_trial_pca = inputs_pca.reshape(
+    #     inputs.shape[0], -1, n_components)
+    #
+    # # shape: (time, trials, pca_components)
+    # inputs = inputs_trial_pca.copy()
+
+    ##############################
+
+    # Add stim time as external input
+    # Shape: (time, trials, 1)
+    stim_time = np.zeros((inputs.shape[0], inputs.shape[1]))
+    stim_time[stim_ind:stim_ind + stim_dur_inds, :] = 1
+
+    # Also add trial number as external input
+    # Scale trial number to be between 0 and 1
+    # trial_num = np.arange(binned_spikes.shape[0])
+    trial_num = np.arange(input_array.shape[0])
+    trial_num_scaled = trial_num / trial_num.max()
+    trial_num_broad = np.broadcast_to(trial_num_scaled, inputs.shape[:2])
+
+    # Shape: (time, trials, pca_components + 1)
+    inputs_plus_context = np.concatenate(
+        [
+            inputs,
+            stim_time[:, :, None],
+            trial_num_broad[:, :, None]
+        ],
+        axis=-1)
+
+    ############################################################
+    # Train model
+    ############################################################
+    if torch.cuda.is_available():
+        device = torch.device("cuda:0")
+        print("Running on the GPU")
+    else:
+        device = torch.device("cpu")
+        print("Running on the CPU")
+
+    input_size = inputs_plus_context.shape[-1]
+    # We don't want to predict the stim time or trial number
+    output_size = inputs_plus_context.shape[-1] - 2
+
+    # Instead of predicting activity in the SAME time-bin,
+    # predict activity in the NEXT time-bin
+    # Forcing the model to learn temporal dependencies
+    forecast_bins = 1
+    inputs_plus_context = inputs_plus_context[:-forecast_bins]
+    inputs = inputs[forecast_bins:]
+
+    # (seq_len * batch, output_size)
+    labels = torch.from_numpy(inputs).type(torch.float32)
+    # (seq_len, batch, input_size)
+    inputs = torch.from_numpy(inputs_plus_context).type(torch.float)
+
+    # Split into train and test
+    train_inds = np.random.choice(
+        np.arange(inputs.shape[1]),
+        int(0.9 * inputs.shape[1]),
+        replace=False)
+    test_inds = np.setdiff1d(np.arange(inputs.shape[1]), train_inds)
+
+    train_inputs = inputs[:, train_inds]
+    train_labels = labels[:, train_inds]
+    test_inputs = inputs[:, test_inds]
+    test_labels = labels[:, test_inds]
+
+    train_inputs = train_inputs.to(device)
+    train_labels = train_labels.to(device)
+    test_inputs = test_inputs.to(device)
+    test_labels = test_labels.to(device)
+
+    ##############################
+    # Train
+    ##############################
+    net = RNN_Wrapper(
+        n_io_layers=3,
+        input_size=input_size,
+        output_size=output_size,
+        shared_rnn=shared_rnn,
+    )
+
+    net.to(device)
+
+    # Append to list
+    net_list.append(net)
+    train_inputs_list.append(train_inputs)
+    train_labels_list.append(train_labels)
+    test_inputs_list.append(test_inputs)
+    test_labels_list.append(test_labels)
+
+
+train_steps = 500_000
 update_steps = 1000
 
-fig = plt.figure()
+fig, ax = plt.subplots(1, 2, figsize=(10, 5), sharey=True)
+ax[0].set_title('Training Loss')
+ax[1].set_title('Cross Validation Loss')
 full_loss = []
 full_cross_val_loss = {}
 for epochs in range(train_steps // update_steps):
-    net, loss, cross_val_loss = train_model(
-        net,
-        train_inputs,
-        train_labels,
-        output_size=output_size,
-        lr=0.001,
-        train_steps=update_steps,
-        criterion=MSELoss(),
-        test_inputs=test_inputs,
-        test_labels=test_labels,
-    )
-    full_loss.extend(loss)
-    # Update keys in cross_val_loss
-    cross_val_loss = {k + epochs * update_steps: v for k,
-                      v in cross_val_loss.items()}
-    full_cross_val_loss.update(cross_val_loss)
+    for i in tqdm(range(len(net_list))):
+        net = net_list[i]
+        train_inputs = train_inputs_list[i]
+        train_labels = train_labels_list[i]
+        test_inputs = test_inputs_list[i]
+        test_labels = test_labels_list[i]
 
-    # Update plot
-    plt.clf()
-    plt.plot(full_loss)
-    plt.plot(list(full_cross_val_loss.keys()),
-             list(full_cross_val_loss.values()))
-    plt.legend(['train', 'test'])
-    plt.xlabel('steps')
-    plt.ylabel('loss')
-    plt.title('Loss')
-    plt.pause(0.01)
+        # Train model
+        net, loss, cross_val_loss = train_model(
+            net,
+            train_inputs,
+            train_labels,
+            output_size=output_size,
+            lr=0.001,
+            train_steps=update_steps,
+            criterion=MSELoss(),
+            test_inputs=test_inputs,
+            test_labels=test_labels,
+        )
+        full_loss.extend(loss)
+        # Update keys in cross_val_loss
+        cross_val_loss = {k + epochs * update_steps: v for k,
+                          v in cross_val_loss.items()}
+        full_cross_val_loss.update(cross_val_loss)
+
+        # Create plot
+        if i == 0:
+            ax[0].plot(full_loss[::10])
+            ax[1].plot(list(full_cross_val_loss.keys())[::10],
+                       list(full_cross_val_loss.values())[::10])
+            plt.pause(0.01)
