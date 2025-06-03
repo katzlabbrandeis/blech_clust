fix: resolve broadcasting issue in check_laser method for laser durations

utils/ephys_data/ephys_data.py
@@ -540,10 +540,10 @@ class ephys_data():
 
             # If it does, pull out laser durations
             if self.laser_durations_exists:
-                self.laser_durations = [dig_in.laser_durations[:]
-                                        for dig_in in dig_in_list]
+                self.laser_durations = np.array([dig_in.laser_durations[:]
+                                                 for dig_in in dig_in_list])
 
-                non_zero_laser_durations = np.sum(self.laser_durations) > 0
+                non_zero_laser_durations = np.any(np.sum(self.laser_durations, axis=0) > 0)
 
             # If laser_durations exists, only non_zero durations
             # will indicate laser