843a67dd29df6521d18acd2a8345add828c1416d
Abuzar Mahmood <abuzarmahmood@gmail.com>
2026-02-12 15:48:59 -0500

    refactor(tests): update `stft_test.py` for data path and import changes
    - Modified import statement to reflect new module path.
    - Commented out previous data directory and updated to use a new data path.
    - Changed the function call to obtain LFPs with an additional parameter for re-extraction.

--- utils/ephys_data/tests/stft_test.py
+++ utils/ephys_data/tests/stft_test.py
@@ -1,11 +1,14 @@
-from ephys_data import ephys_data
+# from ephys_data import ephys_data
+from blech_clust.utils.ephys_data.ephys_data import ephys_data
 import os
 import matplotlib.pyplot as plt
 
-os.chdir('/media/bigdata/firing_space_plot/ephys_data')
+# os.chdir('/media/bigdata/firing_space_plot/ephys_data')
 
-dat = \
-    ephys_data('/media/bigdata/Abuzar_Data/AM11/AM11_4Tastes_191030_114043_copy')
+data_dir = '/media/fastdata/CM74_CTATest2_h2o_nacl_lowqhcl_highqhcl_250614_120546'
+
+# dat = ephys_data('/media/bigdata/Abuzar_Data/AM11/AM11_4Tastes_191030_114043_copy')
+dat = ephys_data(data_dir)
 dat.firing_rate_params = dat.default_firing_params
 
 dat.get_unit_descriptors()
@@ -15,4 +18,5 @@ dat.get_lfps()
 
 dat.get_region_units()
 
+dat.get_lfps(re_extract=True)
 dat.get_stft(recalculate=True)
